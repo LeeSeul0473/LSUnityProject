46434a4c3974706a27c79d12c87d8365c9c0532f
LeeSeul <leeseul2021@duksung.ac.kr>
2025-03-22 18:26:31 +0900

    플레이어, 카메라 이동 구현

--- Assets/Scripts/Player/LSPlayerCamera.cs
+++ Assets/Scripts/Player/LSPlayerCamera.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class LSPlayerCamera : MonoBehaviour
 {
-    public Transform transCamPoint;
+    public Transform objectToFollow;
     [SerializeField]
     private float followSpeed = 10f;
     [SerializeField]
-    private float sensitivity = 100f;
+    private float mousesensitivity = 90f;
     [SerializeField]
     private float scrollSensitivity = 10f;
     [SerializeField]
@@ -17,56 +17,59 @@ public class LSPlayerCamera : MonoBehaviour
     private float rotX;
     private float rotY;
 
-    public Transform transCamera;
-    private Vector3 dir;
-    private Vector3 Raydir;
-    private float dist;
+    public Transform mainCamera;
+    public Vector3 dir;
+    public Vector3 rayDir;
+    public float dist;
 
     [SerializeField]
-    private float minDist;
+    private float minDist = 1f;
     [SerializeField]
-    private float maxDist;
+    private float maxDist = 2f;
     [SerializeField]
     private float smoothness = 10;
 
-    public void Init()
+    public void Start()
     {
         this.rotX = this.gameObject.transform.localRotation.eulerAngles.x;
         this.rotY = this.gameObject.transform.localRotation.eulerAngles.y;
 
-        this.dir = this.transCamera.localPosition.normalized;
-        this.dist = this.transCamera.localPosition.magnitude;
+        this.dir = this.mainCamera.localPosition.normalized;
+        this.dist = this.mainCamera.localPosition.magnitude;
     }
 
     private void Update()
     {
-        // y축을 기준으로 회전
-        this.rotX += -(Input.GetAxis("Mouse Y")) * this.sensitivity + Time.deltaTime;
-        // x축을 기준으로 회전
-        this.rotY += Input.GetAxis("Mouse X") * this.sensitivity + Time.deltaTime;
+    // Rotation Section
+        //Get Input
+        this.rotX += -(Input.GetAxis("Mouse Y")) * this.mousesensitivity + Time.deltaTime;
+        this.rotY += Input.GetAxis("Mouse X") * this.mousesensitivity + Time.deltaTime;
+        
+        //Caculate Rotation
+        this.rotX = Mathf.Clamp(this.rotX, -this.clampAngle, this.clampAngle);
+        Quaternion rot = Quaternion.Euler(this.rotX, this.rotY, 0);
+        this.gameObject.transform.rotation = rot;
+
 
         // 마우스 휠로 카메라 거리를 조절합니다.
         float mouseScroll = Input.GetAxis("Mouse ScrollWheel");
         this.dist -= mouseScroll * this.scrollSensitivity;
         this.dist = Mathf.Clamp(this.dist, minDist, maxDist);
-
-        this.rotX = Mathf.Clamp(this.rotX, -this.clampAngle, this.clampAngle);
-        Quaternion rot = Quaternion.Euler(this.rotX, this.rotY, 0);
-        this.gameObject.transform.rotation = rot;
     }
 
     private void LateUpdate()
     {
-        this.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, this.transCamPoint.position, this.followSpeed * Time.deltaTime);
+        this.transform.position = Vector3.MoveTowards(this.gameObject.transform.position, this.objectToFollow.position, this.followSpeed * Time.deltaTime);
 
         // 카메라와 플레이어 사이의 물체를 감지합니다.
-        this.Raydir = this.gameObject.transform.TransformPoint(this.dir * this.maxDist);
+        this.rayDir = this.gameObject.transform.TransformPoint(this.dir * this.maxDist);
         RaycastHit hit;
-        if (Physics.Linecast(this.gameObject.transform.position, this.Raydir, out hit))
+        if (Physics.Linecast(this.gameObject.transform.position, this.rayDir, out hit))
         {
             this.dist = Mathf.Clamp(hit.distance, this.minDist, this.maxDist);
+            //Debug.Log($"물체감지 {hit.distance}");
         }
 
-        this.transCamera.localPosition = Vector3.Lerp(this.transCamera.localPosition, this.dir * this.dist, Time.deltaTime * this.smoothness);
+        this.mainCamera.localPosition = Vector3.Lerp(this.mainCamera.localPosition, this.dir * this.dist, Time.deltaTime * this.smoothness);
     }
 }
--- Assets/Scripts/Player/LSPlayerMove.cs
+++ Assets/Scripts/Player/LSPlayerMove.cs
@@ -2,15 +2,56 @@ using UnityEngine;
 
 public class LSPlayerMove : MonoBehaviour
 {
+    Camera mainCamera;
+    CharacterController LScontroller;
+    public Transform CameraDir;
+
+    public float walkspeed = 5f;
+    public float runSpeed = 8f;
+    public float speed;
+    public float rotationSpeed = 10f;
+
+    public bool bRun;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        mainCamera = Camera.main;
+        LScontroller = this.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Run Input
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            bRun = true;
+        }
+        else
+        {
+            bRun = false;
+        }
+
+        InputMovement();
+    }
+
+    void InputMovement()
+    {
+        speed = bRun ? runSpeed : walkspeed;
+
+        if((Input.GetAxisRaw("Vertical")!=0) || (Input.GetAxisRaw("Horizontal")!=0))
+        {
+            Vector3 forward = CameraDir.TransformDirection(Vector3.forward);
+            Vector3 right = CameraDir.TransformDirection(Vector3.right);
+            forward.y = 0f;
+            right.y = 0f;
+
+            Vector3 moveDirection = forward * Input.GetAxisRaw("Vertical") + right * Input.GetAxisRaw("Horizontal");
+
+            LScontroller.Move(moveDirection.normalized * speed * Time.deltaTime);
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveDirection), rotationSpeed * Time.deltaTime);
+        }
     }
 }

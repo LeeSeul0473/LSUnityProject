bd338864549ff9c028096e25307aadbff69d45a1
LeeSeul <127869350+LeeSeul0473@users.noreply.github.com>
2025-05-08 13:22:12 +0900

    플레이어 애니메이션 적용중

--- Assets/Scripts/Animation/LSAniminstance.cs
+++ Assets/Scripts/Animation/LSAniminstance.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public class LSAniminstance : MonoBehaviour
 {
     public Animator animator;
+    public GameObject Owner;
+    private CharacterController OwnerController;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        animator->GetComponent<Animator>();
+        animator=GetComponent<Animator>();
+        if(Owner)
+        {
+
+        }
     }
 
     // Update is called once per frame
